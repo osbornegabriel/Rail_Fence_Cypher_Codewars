368a31ba86f296d61835522e19e8380c983f2d41
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-14 12:32:37 -0500

    Write sort_lines method for decoder

--- rail_fence_cypher.rb
+++ rail_fence_cypher.rb
@@ -10,3 +10,21 @@ def rail_encode(s)
   end
   lines.values.reduce(:+)
 end
+
+def rail_decode(s)
+  length = s.length
+  return decode_four(s, length) if length % 4 == 0
+end
+
+def decode_four(s, length)
+  p lines = sort_lines(s,length)
+
+end
+
+def sort_lines(s,length)
+  l_quarter = length/4
+  line_one = s.slice!(0,l_quarter)
+  line_two = s
+  line_three = line_two.slice!(-l_quarter,l_quarter)
+  [line_one, line_two, line_three]
+end
--- runner.rb
+++ runner.rb
@@ -3,6 +3,39 @@ require_relative 'rail_fence_cypher.rb'
 starting_string = "WEAREDISCOVEREDFLEEATONCE"
 encoded_string = "WECRLTEERDSOEEFEAOCAIVDEN"
 
-p starting_string
-p rail_encode(starting_string)
-p rail_encode(starting_string) == encoded_string
+# p starting_string
+# p rail_encode(starting_string)
+# p rail_encode(starting_string) == encoded_string
+
+# p encoded_string.length
+
+p starting_string_two = "HELPMESORTIT"
+
+p encoded_string_two = rail_encode(starting_string_two)
+
+rail_decode(encoded_string_two)
+=begin
+
+H   M.  R
+ E P E O T
+  L   S
+
+H   M.  R
+ E P E O T
+  L   S.  I
+
+H   M.  R
+ E P E O T T
+  L   S.  I
+0,3,9,4, 1,5,10,6  2,7,11,8
+
+0,1,2
+3,4,5,6,7,8
+9,10,11
+
+H   M.  R.  !
+ E P E O T T
+  L   S.  I
+
+=end
+
