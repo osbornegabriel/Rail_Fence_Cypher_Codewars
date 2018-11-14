d98e56213bff0c94e7d6c9dfc93c5aa0aed9791b
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-14 11:52:18 -0500

    Write rail_encoder method

---
+++ rail_fence_cypher.rb
@@ -0,0 +1,13 @@
+starting_string = "WEAREDISCOVEREDFLEEATONCE"
+
+encoded_string = "WECRLTEERDSOEEFEAOCAIVDEN"
+
+def rail_encode(s)
+  lines = {line_one: '', line_two: '', line_three: ''}
+  s.each.with_index |letter, i| do
+    lines[:line_one] << letter if i % 4 == 0
+    lines[:line_three] << letter if i % 4 == 2
+    lines[:line_two] << letter if i % 4 == 1 || i % 4 == 3
+  end
+  h.values.reduce(:+)
+end
