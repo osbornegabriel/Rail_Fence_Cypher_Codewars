626b54198db32e33539d1c45f971217bbd31a10e
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-14 14:20:39 -0500

    Remove unnecessary lines of code

--- rail_fence_cypher.rb
+++ rail_fence_cypher.rb
@@ -14,9 +14,7 @@ end
 def rail_decode(s)
   length = s.length
   lines = sort_into_lines(s,length)
-
-  decoded_message = combine_lines(lines,length)
-  p decoded_message
+  combine_lines(lines,length)
 end
 
 # For when the modulo 4 is 3
@@ -47,10 +45,9 @@ def decode_zero(s, length)
 end
 
 def sort_into_lines(s,length)
-  lines = decode_zero(s, length) if length % 4 == 0
-  lines = decode_two(s, length) if length % 4 == 2 || length % 4 == 1
-  lines = decode_three(s, length) if length % 4 == 3
-  lines
+  return decode_zero(s, length) if length % 4 == 0
+  return decode_two(s, length) if length % 4 == 2 || length % 4 == 1
+  return decode_three(s, length) if length % 4 == 3
 end
 
 def combine_lines(lines, length)
@@ -62,16 +59,5 @@ def combine_lines(lines, length)
     decoded_s << lines[:line_three].slice!(0)
     decoded_s << lines[:line_two].slice!(0)
   end
-  p lines
-  p "this is the decoded"
-  # p decoded_s
   decoded_s + lines.values.reduce(:+)
 end
-
-# def sort_into_lines_one(s,length)
-#   l_quarter = length/4
-#   line_one = s.slice!(0,l_quarter + 1)
-#   line_two = s
-#   line_three = line_two.slice!(-l_quarter,l_quarter)
-#   {line_one: line_one, line_two: line_two, line_three: line_three}
-# end
