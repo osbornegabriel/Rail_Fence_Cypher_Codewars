74998bb7cd0b061dd2a5d78a990fc80207910541
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-26 18:39:50 -0500

    Write find_middle_lines method

--- rail_fence_cypher.rb
+++ rail_fence_cypher.rb
@@ -30,11 +30,77 @@ def build_lines(phrase,rails,rail_indexes)
 end
 
 def rail_decode(s,rail_count)
+  lines = find_lines(s,rail_count)
+  assemble_lines(lines,rail_count)
+end
+
+def assemble_lines(lines,rail_count)
+  decoded_s = ''
+  size_of_middle = rail_count - 2
+  p "inside assemble_lines"
+  p lines
+  lines[:middle_line] = lines[:middle_line][0].join
+  lines[:first_line].size.times do |i|
+    decoded_s += lines[:first_line].slice!(0)
+    decoded_s += lines[:middle_line].slice!(0) || ''
+    decoded_s += lines[:last_line].slice!(0) || ''
+    decoded_s += lines[:middle_line].slice!(0) || ''
+    p decoded_s
+  end
+  decoded_s
+end
+
+def find_lines(s,rail_count)
   phrase = s.dup
   last_line = find_last_line(phrase,rail_count)
   first_line = find_first_line(phrase,rail_count)
-  middle_line = phrase
-  [first_line, middle_line, last_line]
+  middle_line = find_middle_lines(phrase,rail_count-2)
+  {first_line: first_line, middle_line: middle_line, last_line: last_line}
+end
+
+def find_middle_lines(s,number_of_lines)
+  middle_lines = []
+  length = s.length
+  one_sequence = number_of_lines * 2
+  number_of_sequences = length / one_sequence
+  extra_letters_count = length % one_sequence
+  slice_size = number_of_sequences * 2
+
+  if extra_letters_count == 0
+    # 'cool'
+    # all lines are same length
+    number_of_lines.times do |i|
+      middle_lines[i] = s.slice!(0, slice_size).chars
+    end
+  elsif extra_letters_count <= number_of_lines
+    # do nothing
+    number_of_lines.times do |i|
+      middle_lines[i] = s.slice!(0, slice_size).chars
+      p "inside elsif!"
+      if extra_letters_count > 0
+        middle_lines[i].push(s.slice!(0))
+        extra_letters_count -= 1
+      end
+      p middle_lines
+    end
+  elsif extra_letters_count > number_of_lines
+    s.reverse!
+    extra_letters_count /= 2
+    number_of_lines.times do |i|
+      middle_lines[i] = s.slice!(0, slice_size).chars
+      if extra_letters_count > 0
+        middle_lines[i].push(s.slice!(0))
+        extra_letters_count -= 1
+      end
+      middle_lines[i].reverse!
+    end
+  end
+  p "these are your middle lines"
+  p middle_lines
+  # length to slice is the
+  # number_of_sequences * 2
+  # + 1 if there are still extra letters remaining
+
 end
 
 def find_last_line(s,rail_count)
