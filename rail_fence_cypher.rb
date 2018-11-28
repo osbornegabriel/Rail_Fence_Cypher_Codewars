aa2d1ca4663e1b4fa1d0c32bbb145fc6589359d8
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-27 22:19:35 -0500

    variable renaiming for clarity

--- rail_fence_cypher.rb
+++ rail_fence_cypher.rb
@@ -14,9 +14,9 @@ def rail_encode(s,rail_count)
 end
 
 def create_rails(rail_indexes)
-  lines = {}
+  lines = Hash.new
   rail_indexes.each do |i|
-    lines[i] = ''
+    lines[i] = String.new
   end
   lines
 end
@@ -44,11 +44,19 @@ end
 def rail_decode(s,rail_count)
   phrase = s.dup
   lines = find_lines(phrase,rail_count)
-  assemble_lines(lines)
+  decode_lines(lines)
 end
 
-def assemble_lines(lines)
-  decoded_s = ''
+def find_lines(phrase,rail_count)
+  ## the three find_line methods need to be kept in this order for code to properly function ##
+  last_line = find_last_line(phrase,rail_count)
+  first_line = find_first_line(phrase,rail_count)
+  middle_lines = find_middle_lines(phrase,rail_count)
+  {first_line: first_line, middle_lines: middle_lines, last_line: last_line}
+end
+
+def decode_lines(lines)
+  decoded_s = String.new
   lines[:first_line].size.times do
     slice_to_rail(decoded_s,lines[:first_line])
     decoded_s += slice_mid_lines(lines[:middle_lines])
@@ -59,21 +67,13 @@ def assemble_lines(lines)
 end
 
 def slice_mid_lines(middle_lines)
-  letters = ''
+  letters = String.new
   middle_lines.each{|line| slice_to_rail(letters,line)}
   letters
 end
 
 def slice_mid_lines_rev(middle_lines)
   slice_mid_lines(middle_lines.reverse)
-end
-
-def find_lines(phrase,rail_count)
-  ## the three find_line methods need to be kept in this order for code to properly function ##
-  last_line = find_last_line(phrase,rail_count)
-  first_line = find_first_line(phrase,rail_count)
-  middle_lines = find_middle_lines(phrase,rail_count-2)
-  {first_line: first_line, middle_lines: middle_lines, last_line: last_line}
 end
 
 def find_last_line(s,rail_count)
@@ -90,25 +90,25 @@ def find_first_line(s, rail_count)
 end
 
 def find_middle_lines(s,rail_count)
-  one_sequence = rail_count * 2
+  one_sequence = (rail_count-2) * 2
   number_of_sequences = s.length / one_sequence
   extra_letters_count = s.length % one_sequence
   slice_size = number_of_sequences * 2
   if extra_letters_count > rail_count
     rev_divide_mid_lines(s,rail_count,extra_letters_count,slice_size)
   else
-    divide_middle_lines(s,rail_count,extra_letters_count,slice_size)
+    divide_mid_lines(s,rail_count,extra_letters_count,slice_size)
   end
 end
 
 def rev_divide_mid_lines(s,num_of_lines,extra_letters_count,slice_size)
     s.reverse!
     extra_letters_count /= 2
-    middle_lines = divide_middle_lines(s,num_of_lines,extra_letters_count,slice_size)
+    middle_lines = divide_mid_lines(s,num_of_lines,extra_letters_count,slice_size)
     middle_lines.map{|a| a.reverse}
 end
 
-def divide_middle_lines(s,number_of_lines,extra_letters_count,slice_size)
+def divide_mid_lines(s,number_of_lines,extra_letters_count,slice_size)
   middle_lines = Array.new
   number_of_lines.times do |i|
     middle_lines[i] = s.slice!(0, slice_size).chars
